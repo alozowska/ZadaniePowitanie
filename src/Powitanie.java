0d459dd015f32e8019c5f9705abe8bf985ba2264
Agnieszka Łozowska <aga.lozowska@wp.pl>
2018-07-19 14:58:35 +0200

    poprawiona pętla

--- src/Powitanie.java
+++ src/Powitanie.java
@@ -6,12 +6,10 @@ public class Powitanie {
         String[] imiona = new String[5];
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj 5 imion");
-        imiona[0] = scanner.nextLine();
-        imiona[1] = scanner.nextLine();
-        imiona[2] = scanner.nextLine();
-        imiona[3] = scanner.nextLine();
-        imiona[4] = scanner.nextLine();
-        for (int i=imiona.length -1; i>=0; i--){
+        for (int i = 0; i < imiona.length; i++) {
+            imiona[i] = scanner.nextLine();
+        }
+        for (int i = imiona.length - 1; i >= 0; i--) {
             System.out.println("Cześć " + imiona[i]);
 
         }
